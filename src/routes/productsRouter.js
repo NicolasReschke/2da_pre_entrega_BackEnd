26e8b2e1cfe08e39b312266a485fb3cc04bbd478
Nicolas Reschke <nicolas.reschke@hotmail.com>
2024-05-23 19:43:07 -0300

    first commit

---
+++ src/routes/productsRouter.js
@@ -0,0 +1,10 @@
+// productsRouter.js
+import { Router } from 'express';
+import { getProducts, getCategories } from '../controllers/productsController.js';
+
+const router = Router();
+
+router.get('/', getProducts)
+router.get('/categories', getCategories)
+
+export default router;
