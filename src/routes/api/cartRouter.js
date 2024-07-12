23d224d49ef7e92e81e6aa382ab9a65261061d43
Nicolas Reschke <nicolas.reschke@hotmail.com>
2024-07-12 00:52:12 -0300

    Tercera pre entrega commit 1

--- src/routes/api/cartRouter.js
+++ src/routes/api/cartRouter.js
@@ -1,18 +1,23 @@
-import { Router } from 'express'
+import express from 'express'
 import {
     getCart,
+    updateCart,
     addProductToCart,
     updateProductQuantity,
     deleteProductFromCart,
-    deleteAllProductsFromCart
-} from '../controllers/cartController.js'
+    deleteAllProductsFromCart,
+    getPurchase
+} from '../../controllers/cartController.js'
 
-const router = Router()
+const router = express.Router()
 
 router.get('/:cid', getCart)
+router.put('/:cid', updateCart)
 router.post('/:cid/products/:pid', addProductToCart)
 router.put('/:cid/products/:pid', updateProductQuantity)
 router.delete('/:cid/products/:pid', deleteProductFromCart)
 router.delete('/:cid', deleteAllProductsFromCart)
+router.get('/:cid/purchase', getPurchase)
+router.post('/:cid/purchase', getPurchase)
 
 export default router
