ca5c1e02445d7a0af07ee2842173a7064c9449d9
Nicolas Reschke <nicolas.reschke@hotmail.com>
2024-09-04 00:11:12 -0300

    14º commit 1.1

--- src/config/passportConfig.js
+++ src/config/passportConfig.js
@@ -1,7 +1,7 @@
 import passport from 'passport'
 import { Strategy as LocalStrategy } from 'passport-local'
 import { Strategy as GitHubStrategy } from 'passport-github2'
-/* import { Strategy as GoogleStrategy } from 'passport-google-oauth20' */
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20'
 import bcrypt from 'bcryptjs'
 import dotenv from 'dotenv'
 
@@ -30,7 +30,7 @@ const callbackURL = process.env.LOGGER_ENV === 'production'
     ? "http://localhost:8080/auth/github/callback"
     : "https://2dapreentregabackend-production.up.railway.app/auth/github/callback"
 
-const clientID = process.env.LOGGER_ENV === 'production'
+const CLIENT_ID = process.env.LOGGER_ENV === 'production'
     ? process.env.GITHUB_CLIENT_ID_PRODUCTION
     : process.env.GITHUB_CLIENT_ID_DEVELOPMENT
 
@@ -39,7 +39,7 @@ const clientSecret = process.env.LOGGER_ENV === 'production'
     : process.env.GITHUB_CLIENT_SECRET_DEVELOPMENT
 
 passport.use(new GitHubStrategy({
-    clientID: clientID,
+    clientID: CLIENT_ID,
     clientSecret: clientSecret,
     callbackURL: callbackURL
     /* callbackURL: "http://localhost:8080/auth/github/callback" */
@@ -83,7 +83,7 @@ passport.use(new GitHubStrategy({
     }
 }))
 
-/* passport.use(new GoogleStrategy({
+passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "http://localhost:8080/auth/google/callback"
@@ -126,7 +126,7 @@ passport.use(new GitHubStrategy({
     } catch (err) {
         return done(err)
     }
-})) */
+}))
 
 passport.serializeUser((user, done) => {
     done(null, user.id)
