3c9494e42d67ea0e712d3f75e0a9969334b912c3
Nicolas Reschke <nicolas.reschke@hotmail.com>
2024-09-22 17:45:17 -0300

    Proyecto final 1.2

--- src/config/passportConfig.js
+++ src/config/passportConfig.js
@@ -38,11 +38,21 @@ const clientSecret = process.env.LOGGER_ENV === 'production'
     ? process.env.GITHUB_CLIENT_SECRET_PRODUCTION
     : process.env.GITHUB_CLIENT_SECRET_DEVELOPMENT */
 
-passport.use(new GitHubStrategy({
+/* passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID_DEVELOPMENT,
     clientSecret: process.env.GITHUB_CLIENT_SECRET_DEVELOPMENT,
-    /* callbackURL: "http://localhost:8080/auth/github/callback" */
-    callbackURL: "https://2dapreentregabackend-production.up.railway.app/auth/github/callback"
+    callbackURL: "http://localhost:8080/auth/github/callback"
+    callbackURL: "https://2dapreentregabackend-production.up.railway.app/auth/github/callback" */
+passport.use(new GitHubStrategy({
+    clientID: process.env.NODE_ENV === 'production' 
+        ? process.env.GITHUB_CLIENT_ID_PRODUCTION 
+        : process.env.GITHUB_CLIENT_ID_DEVELOPMENT,
+    clientSecret: process.env.NODE_ENV === 'production' 
+        ? process.env.GITHUB_CLIENT_SECRET_PRODUCTION 
+        : process.env.GITHUB_CLIENT_SECRET_DEVELOPMENT,
+    callbackURL: process.env.NODE_ENV === 'production'
+        ? "https://2dapreentregabackend-production.up.railway.app/auth/github/callback"
+        : "http://localhost:8080/auth/github/callback"
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         if (!profile.emails || !profile.emails[0].value) {
