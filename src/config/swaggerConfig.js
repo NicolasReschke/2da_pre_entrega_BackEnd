2ac21eb37dae31bca1e6c2556275d588c6711f1b
Nicolas Reschke <nicolas.reschke@hotmail.com>
2024-09-24 17:44:31 -0300

    Proyecto final 1.3.3

--- src/config/swaggerConfig.js
+++ src/config/swaggerConfig.js
@@ -1,6 +1,11 @@
 import { url } from 'inspector'
 import swaggerJsdoc from 'swagger-jsdoc'
 import swaggerUi from 'swagger-ui-express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const isProduction = process.env.NODE_ENV === 'production'
 
 const options = {
     definition: {
@@ -11,8 +16,9 @@ const options = {
         },
         servers: [
             {
-                url: 'https://2dapreentregabackend-production.up.railway.app',
-                url: 'http://localhost:8080',
+                url: isProduction 
+                    ? "https://localhost:8080"
+                    : "https://2dapreentregabackend-production.up.railway.app",
             },
         ],
     },
